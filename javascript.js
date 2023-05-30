32b89e8aa9275810d7a3b5f35c3b03aa7724735f
David Bishop <133163677+DavidBishop765@users.noreply.github.com>
2023-05-30 15:24:28 -0400

    Create a game with five rounds

--- javascript.js
+++ javascript.js
@@ -1,48 +1,72 @@
-// create a function (getComputerChoice) that will randomly return either 'Rock', 'Paper', or 'Scissors'
+// create function getComputerChoice which randomly chooses Rock, Paper, or Scissors
 function getComputerChoice() {
+// create variable num will produce three distinct options (1, 2, and 3)
     let num = Math.floor(Math.random() * 3 + 1);
+// assign Rock, Paper, and Scissors to each option
     if (num === 1) {
         return 'Rock'
-    } if (num ===2) {
+    } if (num === 2) {
         return 'Paper'
     } else {return 'Scissors'}
 }
 
-// Create function playGame() that plays a single round. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-    let a = 0, b = 0; a=a++;
-    console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-    function playRound(playerSelection, computerSelection = getComputerChoice()) {
-        restlowercase = playerSelection.slice(1).toLowerCase();
-        firstcapital = playerSelection.charAt(0).toUpperCase();
-        playerSelectionSentCase = firstcapital + restlowercase;
+// declare variables for starting scores and round number
+    let playerScore = 0, computerScore = 0, roundNumber = 0;
+// log starting scores of 0 in the console
+    console.log(`Score:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+// create function game() which allows you to play 5 rounds
+function game() {  
+    for (let i = 0; i <= 4; i++) {
+        // create function playRound with parameters playerSelection and computerSelection (default = the function getComputerChoice)
+        function playRound(playerSelection, computerSelection = getComputerChoice()) {
+            // prompt the user for their input and assign it to variable playerSelection
+            playerSelection = window.prompt("Choose your weapon! Rock? Paper? Or Scissors?", "");
+            // make playerSelection case insensitive and store as variable playerSelectionSentCase
+            restlowercase = playerSelection.slice(1).toLowerCase();
+            firstcapital = playerSelection.charAt(0).toUpperCase();
+            playerSelectionSentCase = firstcapital + restlowercase;
         
-        if (playerSelection === computerSelection) {
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return `It's a draw! You both chose ${playerSelectionSentCase}`; 
-        } else if (playerSelectionSentCase === 'Rock' && computerSelection === 'Paper') {
-            b++;
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return 'You Lose! Paper beats Rock';
-        } else if (playerSelectionSentCase === 'Rock' && computerSelection === 'Scissors') {
-            a++;
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return 'You Win! Rock beats Scissors';       
-        } else if (playerSelectionSentCase === 'Paper' && computerSelection === 'Rock') {
-            a++;
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return 'You Win! Paper beats Rock';       
-        } else if (playerSelectionSentCase === 'Paper' && computerSelection === 'Scissors') {
-            b++;
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return 'You Lose! Scissors beats Paper';       
-        } else if (playerSelectionSentCase === 'Scissors' && computerSelection === 'Rock') {
-            b++;
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return 'You Lose! Rock beats Scissors';       
-        } else if (playerSelectionSentCase === 'Scissors' && computerSelection === 'Paper') {
-            a++;
-            console.log(`Score:\nPlayer: ${a}\nComputer: ${b}`);
-            return 'You Win! Scissors beats Paper';       
+            // create an appropriate return message for all 9 possible cases of selections and increment the winner's score
+            if (playerSelectionSentCase === 'Rock' && computerSelection === 'Paper') {
+                computerScore++; 
+                return 'You Lose! Paper beats Rock';
+            } else if (playerSelectionSentCase === 'Rock' && computerSelection === 'Scissors') {
+                playerScore++; 
+                return 'You Win! Rock beats Scissors';       
+            } else if (playerSelectionSentCase === 'Paper' && computerSelection === 'Rock') {
+                playerScore++;
+                return 'You Win! Paper beats Rock';       
+            } else if (playerSelectionSentCase === 'Paper' && computerSelection === 'Scissors') {
+                computerScore++; 
+                return 'You Lose! Scissors beats Paper';       
+            } else if (playerSelectionSentCase === 'Scissors' && computerSelection === 'Rock') {
+                computerScore++; 
+                return 'You Lose! Rock beats Scissors';       
+            } else if (playerSelectionSentCase === 'Scissors' && computerSelection === 'Paper') {
+                playerScore++; 
+                return 'You Win! Scissors beats Paper';       
+            } else {
+                return `It's a draw! You both chose ${playerSelectionSentCase}`
+            }
         }
-    }
+        // increment the round number
+        roundNumber++;
+        // run one round; log the round number, result message, and updated scores
+        result = playRound();
+        console.log(`\nRound #${roundNumber}`);        
+        console.log(result);
+        console.log(`Score:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+    } 
+
+    // after 5 rounds have finished, declare a winner with an appropriate message; reset the scores and round number
+    if (playerScore < computerScore) {
+        playerScore = 0, computerScore = 0, roundNumber = 0;
+        return "You lose! Sucks to suck!"
+    } else if (playerScore > computerScore) {
+        playerScore = 0, computerScore = 0, roundNumber = 0;
+        return "You win! Take it easy on us champ!"
+    } else {
+        playerScore = 0, computerScore = 0, roundNumber = 0;
+        return "You tied?"}
+}
     
