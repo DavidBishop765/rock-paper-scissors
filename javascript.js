0cb06795a6d13b1bbffdd9dab8cda8f7de06bc1e
David Bishop <133163677+DavidBishop765@users.noreply.github.com>
2023-06-02 13:38:07 -0400

    Change format to "Race to Five"; Clean up code

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// define function to randomly generate the computer's choice
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3 + 1);
     if (num === 1) {
@@ -6,20 +7,18 @@ function getComputerChoice() {
         return 'Paper'
     } else {return 'Scissors'}
 }
-let computerSelection;
-let playerScore = 0, computerScore = 0, roundNumber = 1;
-const round = document.querySelector('p');
-const player = document.querySelector('.player');
-const computer = document.querySelector('.computer');
-const computerChoice = document.querySelector('.computerChoice');
-const playerChoice = document.querySelector('playerChoice');
 
-round.textContent = `Next Round: Round ${roundNumber}`;
-player.textContent = `Player Score: ${playerScore}`;
-computer.textContent = `Computer Score: ${computerScore}`;
+// declare variables for use in function for single round
+let playerScore = 0, 
+    computerScore = 0, 
+    roundNumber = 1,
+    computerSelection,
+    playerSelection,
+    restlowercase,
+    firstcapital,
+    playerSelectionSentCase;
 
-
-
+// define function to player a single round and return a result
 function playRound(playerSelection, computerSelection = getComputerChoice()) {
     restlowercase = playerSelection.slice(1).toLowerCase();
     firstcapital = playerSelection.charAt(0).toUpperCase();
@@ -50,35 +49,49 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     } 
 }
 
-const resultsLine = document.querySelector('para1');
-const winner = document.querySelector('.winner');
+// declare variables targeting each element of the HTML
+const round = document.querySelector('.round');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 const roundWinner = document.querySelector('.para2');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+
+// add text to display live round number and scores
+round.textContent = `Next Round: Round ${roundNumber}`;
+player.textContent = `Player Score: ${playerScore}`;
+computer.textContent = `Computer Score: ${computerScore}`;
+
+// define function (and associated variables) to run a full round and update live round number and score
 let weapon;
 let result;
-
 function runRound(weapon) {
     result = playRound(weapon, computerSelection);
     roundWinner.textContent = result;
 
-    if (roundNumber <= 5) {
+    if (playerScore < 5 && computerScore < 5) {
         round.textContent = `Next Round: Round ${roundNumber}`;
         player.textContent = `Player Score: ${playerScore}`;
         computer.textContent = `Computer Score: ${computerScore}`;
-    } else if (roundNumber == 6) {
-        round.textContent = `Game Over`;
-        if (computerScore > playerScore) {
-            round.textContent += ': You lose!';
-        } else if (playerScore > computerScore) {
-            round.textContent += ': You win!';
-        } else {round.textContent += ': You tied!'};
+    } else if (playerScore == 5 || computerScore == 5) {
+        if (round.textContent.includes('Game Over')) {
+            location.reload();
+        } else {
+            round.textContent = `Game Over`;
+            if (computerScore > playerScore) {
+                round.textContent += ': You lose!';
+            } else if (playerScore > computerScore) {
+                round.textContent += ': You win!';
+            } else {round.textContent += ': You tied!'
+        }
         player.textContent = `Player Score: ${playerScore}`;
-        computer.textContent = `Computer Score: ${computerScore}`; 
-    } else {location.reload()}
+        computer.textContent = `Computer Score: ${computerScore}`;
+        } 
+    }
 }
 
-const rock = document.querySelector('.rock');
+// add event listeners to listen for a click on each button and trigger 
 rock.addEventListener('click', () => runRound('Rock'));
-const paper = document.querySelector('.paper');
 paper.addEventListener('click', () => runRound('Paper'));
-const scissors = document.querySelector('.scissors');
 scissors.addEventListener('click', () => runRound('Scissors'));
